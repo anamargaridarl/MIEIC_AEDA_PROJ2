31e34972d587bd6b756febca0e729740277d8c26
andrefmrocha <andrefmrocha@live.com.pt>
2018-12-19 12:23:04 +0000

    Added files for the start of the tech support

---
+++ src/TechSupport.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andrefmrocha on 12/19/18.
+//
+
+#include "TechSupport.h"
---
+++ src/TechSupport.h
@@ -0,0 +1,15 @@
+//
+// Created by andrefmrocha on 12/19/18.
+//
+
+#ifndef AEDA_PROJECT_TECHSUPPORT_H
+#define AEDA_PROJECT_TECHSUPPORT_H
+
+
+class TechSupport {
+
+
+};
+
+
+#endif //AEDA_PROJECT_TECHSUPPORT_H
