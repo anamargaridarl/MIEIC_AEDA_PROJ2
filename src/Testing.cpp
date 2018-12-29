801fc832afc30ad6e13625bc1b71b87229c7e60a
andrefmrocha <andrefmrocha@live.com.pt>
2018-12-29 16:05:24 +0000

    Added test files

---
+++ src/Testing.cpp
@@ -0,0 +1,37 @@
+//
+// Created by andrefmrocha on 10/30/18.
+//
+
+#include "Court.h"
+#include "Person.h"
+#include "Date.h"
+#include "Company.h"
+#include <ostream>
+
+using namespace std;
+int main()
+{
+ /*   Date d(1, 1, 1);
+    Date d2(1, 1, 2);
+    cout << d2 - d;*/
+    Company c;
+    c.addRepairer("Joao", "M");
+    c.addRepairer("Joao", "M");
+    c.addRepairer("Joa", "M");
+    c.scheduleRepair(2, 1, 0);
+    c.scheduleRepair(2, 1, 0);
+    c.scheduleRepair(2, 1, 0);
+    c.scheduleRepair(3, 1, 0);
+    c.listAllRepairers();
+    Supporter sp("Joao", "M");
+    sp.scheduleRepair(Date(2,1,1), Date(1, 1, 1), 0);
+    sp.scheduleRepair(Date(3,1,1), Date(1, 1, 1), 0);
+    sp.scheduleRepair(Date(4,1,1), Date(1, 1, 1), 0);
+    ofstream outfile("Supporter.json");
+    sp.storeInfo(outfile, 0);
+    Supporter sp1;
+    ifstream infile("Supporter.json");
+    sp1.readInfo(infile);
+    ofstream outfile1("Support2.json");
+    sp1.storeInfo(outfile1, 0);
+}
